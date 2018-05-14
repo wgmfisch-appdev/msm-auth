5acfbafa72ac1f0a778852c0dd6af061127e4abc
Arjun Venkataswamy <avenkat2@gmail.com>
2018-05-13 22:52:59 -0500

    changes name to MSM Associations

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TestMsmDebug
+module TestMsmAssociations
   class Application < Rails::Application
     config.generators do |g|
       g.test_framework nil
