8571105cc43c41eaf309c202d39acd86860031d6
Jelani Woods <jmw686@nyu.edu>
2018-11-06 10:58:37 -0600

    bundle update, remove csrf, update project.rake

--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: characters
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  movie_id   :integer
+#  actor_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :character do
     sequence(:name) { |n| "Some fake name #{n}" }
